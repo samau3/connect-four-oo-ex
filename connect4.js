134317ff585dff10f039d26d9dcd5e311b713846
Diogo Botelho <diogofilipebotelho@gmail.com>
2021-08-26 19:00:58 -0500

    debugged handleClick context

--- connect4.js
+++ connect4.js
@@ -11,7 +11,7 @@
   if no arguments are provided, the default values are h = 6 and w = 7
 */
 
-class Game() {
+class Game {
   constructor(height = 6, width = 7) {
     this.height = height;
     this.width = width;
@@ -37,7 +37,7 @@ class Game() {
     // make column tops (clickable area for adding a piece to that column)
     const top = document.createElement('tr');
     top.setAttribute('id', 'column-top');
-    top.addEventListener('click', handleClick);
+    top.addEventListener('click', this.handleClick.bind(this));
 
     for (let x = 0; x < this.width; x++) {
       const headCell = document.createElement('td');
@@ -61,17 +61,6 @@ class Game() {
     }
   }
 
-  /** findSpotForCol: given column x, return top empty y (null if filled) */
-
-  findSpotForCol(x) {  // FOR LATER REVIEW TO SEE IF PARAMETER STILL WORKS
-    for (let y = this.height - 1; y >= 0; y--) {
-      if (!this.board[y][x]) {
-        return y;
-      }
-    }
-    return null;
-  }
-
   /** placeInTable: update DOM to place piece into HTML table of board */
 
   placeInTable(y, x) { // FOR LATER REVIEW TO SEE IF PARAMETER STILL WORKS
@@ -91,37 +80,43 @@ class Game() {
   }
 
   /** handleClick: handle click of column top to play piece */
-
+  
   handleClick(evt) {
+    console.log(this);
     // get x from ID of clicked cell
     const x = +evt.target.id;
 
     // get next spot in column (if none, ignore click)
-    const y = findSpotForCol(x);
+    const y = this.findSpotForCol(x);
+    //const y = () => this.findSpotForCol(x) ;
+
     if (y === null) {
       return;
     }
 
     // place piece in board and add to HTML table
     this.board[y][x] = this.currPlayer;
-    placeInTable(y, x);
+    this.placeInTable(y, x);
 
     // check for win
-    if (checkForWin()) {
-      return endGame(`Player ${this.currPlayer} won!`);
+    if (this.checkForWin()) {
+      return this.endGame(`Player ${this.currPlayer} won!`);
     }
 
     // check for tie
     if (this.board.every(row => row.every(cell => cell))) {
-      return endGame('Tie!');
+      return this.endGame('Tie!');
     }
 
     // switch players
     this.currPlayer = this.currPlayer === 1 ? 2 : 1;
   }
 
+
   checkForWin() {
+    console.log("checkForWin=",this);
     function _win(cells) {
+      console.log("win=",this);
       // Check four cells to see if they're all color of current player
       //  - cells: list of four (y, x) cells
       //  - returns true if all are legal coordinates & all match currPlayer
@@ -146,16 +141,28 @@ class Game() {
         const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
         // find winner (only checking each win-possibility as needed)
-        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+        if (_win.bind(this,horiz) || _win.bind(this,vert) || _win.bind(this,diagDR) || _win.bind(this,diagDL)) {
           return true;
         }
       }
     }
   }
 
+
+  /** findSpotForCol: given column x, return top empty y (null if filled) */
+
+  findSpotForCol(x) {  // FOR LATER REVIEW TO SEE IF PARAMETER STILL WORKS
+    for (let y = this.height - 1; y >= 0; y--) {
+      if (!this.board[y][x]) {
+        return y;
+      }
+    }
+    return null;
+  }
 }
 
+const game = new Game(6,7);
+game.makeBoard();
+game.makeHtmlBoard();
 
-makeBoard();
-makeHtmlBoard();
 
